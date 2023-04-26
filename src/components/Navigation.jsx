4c63c51fc27edb7af6e1f1108aa52c2f9150071c
khalexiiii <alexandrite.llarenas@gmail.com>
2023-04-26 12:49:14 +0800

    Add Logo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -17,32 +17,34 @@ function Navigation() {
 
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand as={Link} to="/">The Book Bower</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/about">About</Nav.Link>
-          <Nav.Link as={Link} to="/bookspage">Books</Nav.Link>
-          {isLoggedIn && (
-            <>
-              <Nav.Link as={Link} to="/searchbar">Search Books</Nav.Link>
-              <Nav.Link as={Link} to={isAdmin ? '/admin' : '/user'}>Dashboard</Nav.Link>
-            </>
-          )}
-        </Nav>
-        <Nav>
-          {isLoggedIn ? (
-            <>
-              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-            </>
-          ) : (
-            <>
-              <Nav.Link as={Link} to="/login">Login</Nav.Link>
-              <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
-            </>
-          )}
-        </Nav>
-      </Navbar.Collapse>
+      <Container>
+        <Navbar.Brand as={Link} to="/"><img src={Logo}alt="the-book-bower.png" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/bookspage">Books</Nav.Link>
+            {isLoggedIn && (
+              <>
+                <Nav.Link as={Link} to="/searchbar">Search Books</Nav.Link>
+                <Nav.Link as={Link} to={isAdmin ? '/admin' : '/user'}>Dashboard</Nav.Link>
+              </>
+            )}
+          </Nav>
+          <Nav>
+            {isLoggedIn ? (
+              <>
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }

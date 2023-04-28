41fcecd9b6aec7c3f32ad9f4ee6ed11d529c41e1
khalexiiii <alexandrite.llarenas@gmail.com>
2023-04-28 09:11:08 +0800

    Login page Update

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -71,6 +71,12 @@ function Login() {
                       <button type="submit" className="form-control btn btn-primary rounded submit px-3">Log In</button>
                     </div>
                     <div className="form-group d-md-flex">
+                      <div className="w-50 text-left">
+                        <label className="checkbox-wrap checkbox-primary mb-0">Remember Me
+                          <input type="checkbox"></input>
+                          <span className="checkmark"></span>
+                        </label>
+                      </div>
                       <div className="w-50 text-md-right">
                         <a href="#">Forgot Password</a>
                       </div>

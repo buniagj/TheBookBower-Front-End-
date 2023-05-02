42d8a9884ee26d3c5b124cf641d29b1db87d51f4
buniagj <dacudagj@gmail.com>
2023-05-02 11:20:33 +0800

    revised User Dashboard

--- src/pages/Dashboard/UserDashboard/User.jsx
+++ src/pages/Dashboard/UserDashboard/User.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import BookTable from '../BookTable';
-
+import defaultProfilePhoto from '../../../assets/Alexandrite.png';
 
 function UserDashboard() {
   const [user, setUser] = useState({});
@@ -9,7 +9,8 @@ function UserDashboard() {
   const [returnedBooks, setReturnedBooks] = useState([]);
   const [editMode, setEditMode] = useState(false);
   const [updatedUser, setUpdatedUser] = useState({ name: '', email: '' });
-
+  const [isUploadingPhoto, setIsUploadingPhoto] = useState(false);
+  
   useEffect(() => {
     // Fetch user data from the server
     axios.get('/api/user')
@@ -66,36 +67,78 @@ function UserDashboard() {
     setUpdatedUser({ ...updatedUser, [name]: value });
   }
 
+  function handlePhotoUpload(event) {
+    setIsUploadingPhoto(true);
+    const formData = new FormData();
+    formData.append('photo', event.target.files[0]);
+
+    axios.post('/api/user/photo', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+      .then(response => {
+        setUser(response.data);
+        setIsUploadingPhoto(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setIsUploadingPhoto(false);
+      });
+  }
+
   return (
     <div>
       <h1>User Dashboard</h1>
-      {editMode ? (
-        <>
-          <label>
-            Name:
-            <input type="text" name="name" value={updatedUser.name} onChange={handleInputChange} required />
-          </label>
-          <label>
-            Email:
-            <input type="email" name="email" value={updatedUser.email} onChange={handleInputChange} required />
-          </label>
-          <button type="button" onClick={handleSave}>Save</button>
-          <button type="button" onClick={handleCancel}>Cancel</button>
-        </>
-      ) : (
-        <>
-          <p>Welcome, {user.name}!</p>
-          <p>Your email address is: {user.email}</p>
-          <p>You joined on: {user.created_at}</p>
-          <button type="button" onClick={handleEdit}>Edit</button>
-        </>
-      )}
-
-      <h2>Borrowed Books</h2>
-      <BookTable books={borrowedBooks} />
-
-      <h2>Returned Books</h2>
-      <BookTable books={returnedBooks} />
+      <div className="user-info">
+        <div className="profile-photo">
+          <img
+            src={user.profile_photo_url || defaultProfilePhoto}
+            alt="Profile"
+            onClick={() => document.getElementById('photo-upload').click()}
+          />
+          <input
+            type="file"
+            id="photo-upload"
+            accept="image/*"
+            onChange={handlePhotoUpload}
+            style={{ display: 'none' }}
+          />
+          {isUploadingPhoto && <p>Uploading...</p>}
+        </div>
+        <div className="user-details">
+          {editMode ? (
+            <>
+              <label>
+                Name:
+                <input type="text" name="name" value={updatedUser.name} onChange={handleInputChange} required />
+              </label>
+              <label>
+                Email:
+                <input type="email" name="email" value={updatedUser.email} onChange={handleInputChange} required />
+              </label>
+              <div className="buttons">
+                <button onClick={handleCancel}>Cancel</button>
+                <button onClick={handleSave}>Save</button>
+              </div>
+            </>
+          ) : (
+            <>
+              <h2>{user.name}</h2>
+              <p>{user.email}</p>
+              <button onClick={handleEdit}>Edit</button>
+            </>
+          )}
+        </div>
+      </div>
+      <div className="borrowed-books">
+        <h2><i className="fas fa-book"></i> Borrowed Books</h2>
+        <BookTable books={borrowedBooks} />
+      </div>
+      <div className="returned-books">
+        <h2><i className="fas fa-history"></i> Returned Books</h2>
+        <BookTable books={returnedBooks} />
+      </div>
     </div>
   );
 }

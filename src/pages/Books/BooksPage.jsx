27c7174c693ce47132a92923093e67db9bc083f3
khalexiiii <alexandrite.llarenas@gmail.com>
2023-05-12 09:22:31 +0800

    Update Books Page

--- src/pages/Books/BooksPage.jsx
+++ src/pages/Books/BooksPage.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Container } from 'react-bootstrap';
 import SearchBar from './SearchBar';
 import BookListings from './BookListings';
 import BookDetails from './BookDetails';
+import './BooksPage.css';
 
 function BooksPage() {
   const { id } = useParams();
@@ -14,10 +17,17 @@ function BooksPage() {
   };
 
   return (
-    <div>
-      <SearchBar onSearch={handleSearch} />
-      <BookListings query={query} />
-      {id && <BookDetails id={id} />}
+    <div className="books-page">
+      <Breadcrumb>
+          <Container>
+            <div className="category-page-title">Books</div>
+          </Container>
+      </Breadcrumb>
+      <Container>
+        <SearchBar onSearch={handleSearch} />
+        <BookListings query={query} />
+        {id && <BookDetails id={id} />}
+      </Container>
     </div>
   );
 }
